5da33e833484d54aa08851d431fdd74c1d1738c1
Chrisdigity <chrisdigity@gmail.com>
2021-08-03 18:24:26 +1000

    implement standardized FilesystemWatcher for both files AND directories

---
+++ src/apiFilesystemWatcher.js
@@ -0,0 +1,86 @@
+/**
+ *  apiFilesystemWatcher.js; Filesystem watcher for MochiMap
+ *  Copyright (C) 2021  Chrisdigity
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published
+ *  by the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+/* modules and utilities */
+const fs = require('fs');
+
+/* FilesystemWatcher */
+class FilesystemWatcher {
+  init (path, options, callback) {
+    // initialize cleanup crew
+    if (!this.cleanupInitialized) {
+      this.cleanupInitialized = true;
+      process.on('SIGINT', this.cleanup);
+      process.on('SIGTERM', this.cleanup);
+    } // end if (!this.cleanupInitialized...
+    try { // try ... perform initial stat of path
+      fs.stat(path, this.handleStat.bind(this, path, 'init', null, callback));
+      if (!options.scanOnly) { // try ... watching for changes on path
+        fs.watch(path, function (eventType, filename) {
+          fs.stat(path, this.handleStat.bind(
+            this, path, eventType, filename, callback));
+        }).on('error', this.reinit.bind(this, path, options, callback));
+        console.log(`// INIT: ${path} watcher started...`);
+      } // end if (!options.scanOnly...
+    } catch (error) { // an error occurred initializing watcher, report/retry
+      this.reinit(path, options, callback, error);
+    } // end try...
+  } // end init...
+
+  handleStat (path, eventType, filename, callback, error, stats) {
+    const logerr = (err) => console.error(`// WATCHER ERROR: ${path}, ${err}`);
+    if (error) logerr(error); else {
+      switch (true) { // check fs.Dirent type
+        case stats.isDirectory():
+          if (eventType === 'init') {
+            return fs.promises.readdir(path).then((files) => {
+              return files.map((file) => require('path').join(path, file));
+            }).then((files) => fs.stat(files, function (err, stats) {
+              if (err) logerr(err); else callback(eventType, filename, stats);
+            })).catch(logerr); // end return fs.promises.readdir...
+          } // end if (eventType...
+        case stats.isFile(): // eslint-disable-line no-fallthrough
+          return callback(eventType, filename, stats);
+        case stats.isSymbolicLink():
+        case stats.isFIFO():
+        case stats.isSocket():
+        case stats.isCharacterDevice():
+        case stats.isBlockDevice():
+          return logerr('Dirent must describe a file or directory');
+        default: // unknown Dirent type
+          return logerr('unknown Dirent type');
+      } // end switch (true...
+    } // end if (error... else...
+  } // end handleStat...
+
+  reinit (path, options, callback, error) {
+    console.error(`// WATCHER ERROR: ${path}, ${error}`);
+    console.error('// INIT: reinitializing in 60 seconds...');
+    this._timeout =
+      setTimeout(this.init.bind(this, path, options, callback), 60 * 1000);
+  } // end reinit...
+
+  cleanup () {
+    if (!this._timeout) return;
+    console.log(`// CLEANUP: terminating ${this.target} watcher timeout...`);
+    clearTimeout(this._timeout);
+  } // end cleanup...
+} // end class FilesystemWatcher...
+
+module.exports = FilesystemWatcher;
