66c8930c9332602a7ce843dac299f600ab4025aa
Chrisdigity <chrisdigity@gmail.com>
2021-08-08 10:08:39 +1000

    improve efficiency of memProcessor IO

--- src/memProcessor.js
+++ src/memProcessor.js
@@ -46,31 +46,32 @@ let MEMPOS = 0; // stores last position in MEMPATH
 const Watcher = new FilesystemWatcher();
 
 /* routines */
-const fileHandler = async (stats, eventType) => {
+const fileHandler = (stats, eventType) => {
+  const time0 = Math.floor(Date.now() / 1000);
   // 'rename' events trigger MEMPOS reset ONLY; events missing stats are ignored
   if (eventType === 'rename') MEMPOS = 0;
   if (eventType === 'rename' || !stats) return;
-  let filehandle; // declare file handle for reading mempool
-  try { // determine if MEMPOOL has valid bytes to read
-    const { length } = Mochimo.TXEntry;
-    const { size } = stats;
-    // check mempool for filesize reduction, reset MEMPOS
-    if (size < MEMPOS) MEMPOS = 0;
-    // ensure mempool has data
-    let position = MEMPOS;
-    const remainingBytes = size - position;
-    if (remainingBytes) {
-      // ensure remainingBytes is valid factor of TXEntry.length
-      const invalidBytes = remainingBytes % length;
-      if (invalidBytes) { // report error in position or (likely) filesize
-        const details = { size, position, remainingBytes, invalidBytes };
-        throw new Error(`MEMPOOL invalid, ${JSON.stringify(details)}`);
-      } else MEMPOS = size; // adjust MEMPOS to size
-      // obtain mempool filehandle
-      filehandle = await fs.promises.open(MEMPOOLPATH);
-      // read chunk of data into buffer
-      const buffer = Buffer.alloc(remainingBytes);
-      const result = await filehandle.read({ buffer, position });
+  // determine if MEMPOOL has valid bytes to read
+  const { length } = Mochimo.TXEntry;
+  const { size } = stats;
+  // check mempool for filesize reduction, reset MEMPOS
+  if (size < MEMPOS) MEMPOS = 0;
+  // ensure mempool has data
+  let position = MEMPOS;
+  const remainingBytes = size - position;
+  if (remainingBytes) {
+    // ensure remainingBytes is valid factor of TXEntry.length
+    const invalidBytes = remainingBytes % length;
+    if (invalidBytes) { // report error in position or (likely) filesize
+      const details = { size, position, remainingBytes, invalidBytes };
+      return console.trace(`MEMPOOL invalid, ${JSON.stringify(details)}`);
+    } else MEMPOS = size; // adjust MEMPOS to size
+    // obtain mempool filehandle
+    let filehandle;
+    fs.promises.open(MEMPOOLPATH).then((handle) => {
+      filehandle = handle; // store handle and read chunk of data into buffer
+      return handle.read({ buffer: Buffer.alloc(remainingBytes), position });
+    }).then((result) => {
       // ensure sufficient bytes were read
       if (result.bytesRead !== remainingBytes) {
         const details = JSON.stringify({ position, result });
@@ -80,31 +81,30 @@ const fileHandler = async (stats, eventType) => {
         const txebuffer = result.buffer.slice(position, position + length);
         const txentry = new Mochimo.TXEntry(txebuffer);
         const txid = txentry.txid;
+        const _txid = txid.slice(0, 8);
         const _id = Db.util.id.transaction(-1, -1, txid);
-        try { // update database with transaction entry
-          if (!(await Db.has('transaction', -1, -1, txid))) {
-            const updateArgs = [ // arguments for Db.update operation
-              Db.util.filterBigInt({ // [BigInt filtered update, query, options]
-                $setOnInsert: { _id, ...txentry.toJSON(true) }
-                /* Using a $setOnInsert update in this manner allows us to
-                 * avoid an uncontrolled condition where a memProcessor synced
-                 * to a node from one server inserts an unconfirmed transaction
-                 * AFTER the same confirmed transaction is inserted by a
-                 * bcProcessor synced to a node from another server. */
-              }), { txid }, { upsert: true }
-            ]; // setOnInsert txentry JSON to transaction database
-            if (await Db.update('transaction', ...updateArgs)) {
-              console.log('TxID', txid.slice(0, 8), 'processed!');
-            } else console.log('TxID', txid.slice(0, 8), 'denied...');
-          } else console.log('TxID', txid.slice(0, 8), 'already exists...');
-        } catch (error) { console.error(txid.slice(0, 8), error); }
+        // check database for existing transaction entry (asynchronous)
+        Db.has('transaction', -1, -1, txid).then((found) => {
+          if (found) return -1;
+          return Db.update('transaction', Db.util.filterBigInt({
+            $setOnInsert: { _id, time0, ...txentry.toJSON(true) }
+            /* Using a $setOnInsert update in this manner allows us to
+            * avoid an uncontrolled condition where a memProcessor synced
+            * to a node from one server inserts an unconfirmed transaction
+            * AFTER the same confirmed transaction is inserted by a
+            * bcProcessor synced to a node from another server. */
+          }), { txid }, { upsert: true });
+        }).then((result) => {
+          if (result > 0) return console.log('TxID', _txid, 'processed!');
+          if (result === 0) return console.log('TxID', _txid, 'denied...');
+          return console.log('TxID', _txid, 'already exists...');
+        }).catch((error) => { console.error(_txid, error); });
       } // end for (position...
-    } // end if (remainingBytes...
-  } catch (error) { // trace error for troubleshooting
-    console.trace(error);
-  } finally { // ensure filehandle is closed after use
-    if (filehandle) await filehandle.close();
-  } // end try... catch... finally...
+    }).catch(console.trace).finally(() => {
+      // ensure filehandle gets closed
+      if (filehandle) filehandle.close();
+    }); // end fs.promises.open... catch... finally...
+  } // end if (remainingBytes...
 }; // end const fileHandler...
 
 /* initialize watcher */
